b1d5bd8ae8638c01bf858bfb307e5ce120cb8f67
Rounit Ranjan Sinha <rounitrsinha@gmail.com>
2023-07-12 16:05:47 +0530

    added

---
+++ background.js
@@ -0,0 +1,64 @@
+let startTime;
+let intervalId;
+let panelWindowId;
+
+chrome.runtime.onStartup.addListener(() => {
+  startTime = Date.now();
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+  startPopupTimer();
+});
+
+function startPopupTimer() {
+  intervalId = setInterval(() => {
+    showPopup();
+  }, 20000);
+}
+
+function stopPopupTimer() {
+  clearInterval(intervalId);
+}
+
+function showPopup() {
+  if (panelWindowId) {
+    chrome.windows.update(panelWindowId, { focused: true });
+  } else {
+    chrome.windows.create(
+      {
+        url: "devwell.html",
+        type: "panel",
+        width: 340,
+        height: 560,
+      },
+      (window) => {
+        panelWindowId = window.id;
+      }
+    );
+  }
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message === "getStartTime") {
+    sendResponse({ startTime: startTime });
+  }
+  if (message === "popupClosed") {
+    startPopupTimer();
+  }
+});
+
+chrome.windows.onRemoved.addListener((windowId) => {
+  if (windowId === panelWindowId) {
+    panelWindowId = null;
+  }
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+  startTime = Date.now();
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message === "getStartTime") {
+    sendResponse({ startTime: startTime });
+  }
+});
---
+++ contentScript.js
@@ -0,0 +1,79 @@
+let imageIndex = 0;
+const imageUrls = [
+  {
+    url: "image1.gif",
+    text: "Relaxes the neck pain due to continous work.",
+  },
+  {
+    url: "image2.gif",
+    text: "Increase the blood flow in your arm and reduce soreness.",
+  },
+  {
+    url: "image3.gif",
+    text: "This stretch is also known as the rhomboid upper or upper back stretch.",
+  },
+  {
+    url: "image4.gif",
+    text: "Regain your mobility and full range of motion and flexibility of the neck muscles.",
+  },
+  {
+    url: "image5.gif",
+    text: "This frees your glute muscles and relaxes the waist pain.",
+  },
+];
+
+let startTime = null;
+let intervalId = null;
+
+function updateImage() {
+  const image = document.getElementById("carousel-image");
+  image.src = chrome.runtime.getURL(imageUrls[imageIndex].url);
+
+  const paragraph = document.getElementById("carousel-paragraph");
+  paragraph.textContent = imageUrls[imageIndex].text;
+}
+
+function getRandomImageIndex() {
+  imageIndex = Math.floor(Math.random() * imageUrls.length);
+}
+
+// ...
+
+function updateActiveTime() {
+  const currentTime = new Date();
+  const activeTime = Math.floor((currentTime - startTime) / 1000); // in seconds
+
+  const hours = Math.floor(activeTime / 3600);
+  const minutes = Math.floor((activeTime % 3600) / 60);
+  const seconds = activeTime % 60;
+
+  const formattedTime = `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+
+  const activeTimeElement = document.getElementById("active-time");
+  activeTimeElement.textContent = `Surfing Time: ${formattedTime}`;
+}
+
+// ...
+
+document.addEventListener("DOMContentLoaded", () => {
+  getRandomImageIndex();
+  updateImage();
+
+  chrome.runtime.sendMessage("getStartTime", (response) => {
+    startTime = new Date(response.startTime);
+    updateActiveTime();
+    intervalId = setInterval(updateActiveTime, 1000); // update every second
+  });
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message === "nextImage") {
+    getRandomImageIndex();
+    updateImage();
+  }
+  if (message === "popupClosed") {
+    clearInterval(intervalId);
+  }
+});
